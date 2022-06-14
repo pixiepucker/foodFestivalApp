a179d162ab563c8ea0ec93f743dec4ddd172ae36
pixiepucker <autumnoffic@gmail.com>
2022-06-13 23:45:09 -0500

    code split js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const webpack = require("webpack")
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: './assets/js/script.js',
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    path: path.join(__dirname + '/dist'),
+    filename: 'main.bundle.js',
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    }),
   ],
-  mode: "development"
+  mode: 'development',
 };
